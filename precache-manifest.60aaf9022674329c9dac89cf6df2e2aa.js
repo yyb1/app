a5aa697f04e499f5a9c680d149f8f76c2054af5d
Diamondstrip <943116216@qq.com>
2019-12-26 18:23:02 +0800

    2

---
+++ precache-manifest.60aaf9022674329c9dac89cf6df2e2aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293b7ea11220470f5cd8b33db8909c97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb7375010d20f439e5b6",
+    "url": "/static/css/main.67cf94c5.chunk.css"
+  },
+  {
+    "revision": "92a455d138545e280e2b",
+    "url": "/static/js/2.c29e0feb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c29e0feb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb7375010d20f439e5b6",
+    "url": "/static/js/main.23304d71.chunk.js"
+  },
+  {
+    "revision": "0ca3b96f01ca27b4cb7a",
+    "url": "/static/js/runtime-main.b0df0266.js"
+  }
+]);
